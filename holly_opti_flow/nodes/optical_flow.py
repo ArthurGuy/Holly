4e6790af1efddf3527946280bb53c989e1ba9026
Arthur Guy <arthur@assys.net>
2018-07-06 23:12:37 +0100

    .

--- holly_opti_flow/nodes/optical_flow.py
+++ holly_opti_flow/nodes/optical_flow.py
@@ -1,71 +1,62 @@
 #!/usr/bin/env python
 import rospy
-import tf
-import time
 import traceback
-import math
-import sys, getopt
 import gpiozero
 from time import sleep
-
 
 from geometry_msgs.msg import Twist, Pose, Point
 from nav_msgs.msg import Odometry
 
-
-PIN_MOUSECAM_CS    = 5
+PIN_MOUSECAM_CS = 5
 PIN_MOUSECAM_RESET = 6
 
-
-ADNS3080_PIXELS_X             = 30
-ADNS3080_PIXELS_Y             = 30
-ADNS3080_COUNTS_PER_INCH      = 1600
+ADNS3080_PIXELS_X = 30
+ADNS3080_PIXELS_Y = 30
+ADNS3080_COUNTS_PER_INCH = 1600
 # This will need trial and error as its dependent on the mounting height
-ADNS3080_COUNTS_PER_METER     = 1
+ADNS3080_COUNTS_PER_METER = 1
 
-
-ADNS3080_PRODUCT_ID           = 0x00
-ADNS3080_REVISION_ID          = 0x01
-ADNS3080_MOTION               = 0x02
-ADNS3080_DELTA_X              = 0x03
-ADNS3080_DELTA_Y              = 0x04
-ADNS3080_SQUAL                = 0x05
-ADNS3080_PIXEL_SUM            = 0x06
-ADNS3080_MAXIMUM_PIXEL        = 0x07
-ADNS3080_CONFIGURATION_BITS   = 0x0a
-ADNS3080_EXTENDED_CONFIG      = 0x0b
-ADNS3080_DATA_OUT_LOWER       = 0x0c
-ADNS3080_DATA_OUT_UPPER       = 0x0d
-ADNS3080_SHUTTER_LOWER        = 0x0e
-ADNS3080_SHUTTER_UPPER        = 0x0f
-ADNS3080_FRAME_PERIOD_LOWER   = 0x10
-ADNS3080_FRAME_PERIOD_UPPER   = 0x11
-ADNS3080_MOTION_CLEAR         = 0x12
-ADNS3080_FRAME_CAPTURE        = 0x13
-ADNS3080_SROM_ENABLE          = 0x14
-ADNS3080_FRAME_PERIOD_MAX_BOUND_LOWER     = 0x19
-ADNS3080_FRAME_PERIOD_MAX_BOUND_UPPER     = 0x1a
-ADNS3080_FRAME_PERIOD_MIN_BOUND_LOWER     = 0x1b
-ADNS3080_FRAME_PERIOD_MIN_BOUND_UPPER     = 0x1c
-ADNS3080_SHUTTER_MAX_BOUND_LOWER          = 0x1e
-ADNS3080_SHUTTER_MAX_BOUND_UPPER          = 0x1e
-ADNS3080_SROM_ID              = 0x1f
-ADNS3080_OBSERVATION          = 0x3d
-ADNS3080_INVERSE_PRODUCT_ID   = 0x3f
-ADNS3080_PIXEL_BURST          = 0x40
-ADNS3080_MOTION_BURST         = 0x50
-ADNS3080_SROM_LOAD            = 0x60
-ADNS3080_PRODUCT_ID_VAL       = 0x17
-
+ADNS3080_PRODUCT_ID = 0x00
+ADNS3080_REVISION_ID = 0x01
+ADNS3080_MOTION = 0x02
+ADNS3080_DELTA_X = 0x03
+ADNS3080_DELTA_Y = 0x04
+ADNS3080_SQUAL = 0x05
+ADNS3080_PIXEL_SUM = 0x06
+ADNS3080_MAXIMUM_PIXEL = 0x07
+ADNS3080_CONFIGURATION_BITS = 0x0a
+ADNS3080_EXTENDED_CONFIG = 0x0b
+ADNS3080_DATA_OUT_LOWER = 0x0c
+ADNS3080_DATA_OUT_UPPER = 0x0d
+ADNS3080_SHUTTER_LOWER = 0x0e
+ADNS3080_SHUTTER_UPPER = 0x0f
+ADNS3080_FRAME_PERIOD_LOWER = 0x10
+ADNS3080_FRAME_PERIOD_UPPER = 0x11
+ADNS3080_MOTION_CLEAR = 0x12
+ADNS3080_FRAME_CAPTURE = 0x13
+ADNS3080_SROM_ENABLE = 0x14
+ADNS3080_FRAME_PERIOD_MAX_BOUND_LOWER = 0x19
+ADNS3080_FRAME_PERIOD_MAX_BOUND_UPPER = 0x1a
+ADNS3080_FRAME_PERIOD_MIN_BOUND_LOWER = 0x1b
+ADNS3080_FRAME_PERIOD_MIN_BOUND_UPPER = 0x1c
+ADNS3080_SHUTTER_MAX_BOUND_LOWER = 0x1e
+ADNS3080_SHUTTER_MAX_BOUND_UPPER = 0x1e
+ADNS3080_SROM_ID = 0x1f
+ADNS3080_OBSERVATION = 0x3d
+ADNS3080_INVERSE_PRODUCT_ID = 0x3f
+ADNS3080_PIXEL_BURST = 0x40
+ADNS3080_MOTION_BURST = 0x50
+ADNS3080_SROM_LOAD = 0x60
+ADNS3080_PRODUCT_ID_VAL = 0x17
 
 # Setup the connection to the optical flow sensor
 opti_flow_sensor = gpiozero.SPIDevice(port=0, device=0)
-opti_flow_reset  = gpiozero.LED(PIN_MOUSECAM_RESET)
-opti_flow_cs     = gpiozero.LED(PIN_MOUSECAM_CS)
+opti_flow_reset = gpiozero.LED(PIN_MOUSECAM_RESET)
+opti_flow_cs = gpiozero.LED(PIN_MOUSECAM_CS)
 
 # Setup the ROS publisher
-rospy.init_node('holly_optical_flow') #public display name of the publisher
-rate = rospy.Rate(10) # 10hz
+rospy.init_node('holly_optical_flow')  # public display name of the publisher
+rate = rospy.Rate(10)  # 10hz
 
 # setup publisher and classes
 odomPub = rospy.Publisher('optical_flow/odom', Odometry, queue_size=10)
@@ -79,34 +70,37 @@ abs_y = 0
 abs_x_m = 0
 abs_y_m = 0
 
+
 def sensor_reset():
     opti_flow_reset.on()
-    sleep(0.010) # reset pulse >10us
+    sleep(0.010)  # reset pulse >10us
     opti_flow_reset.off()
-    sleep(0.040) # 35ms from reset to functional
+    sleep(0.040)  # 35ms from reset to functional
+
 
 def sensor_init():
     opti_flow_cs.on()
 
     sensor_reset()
 
-    pid = mousecam_read_reg(ADNS3080_PRODUCT_ID)
+    pid = sensor_read_reg(ADNS3080_PRODUCT_ID)
     if pid != ADNS3080_PRODUCT_ID_VAL:
         return -1
 
-    #turn on sensitive mode, 1600 counts per inch
-    mousecam_write_reg(ADNS3080_CONFIGURATION_BITS, 0x19)
+    # turn on sensitive mode, 1600 counts per inch
+    sensor_write_reg(ADNS3080_CONFIGURATION_BITS, 0x19)
     return 0
 
 
-def mousecam_write_reg(reg, val):
+def sensor_write_reg(reg, val):
     opti_flow_cs.off()
     opti_flow_sensor._spi.transfer([reg | 0x80])
     opti_flow_sensor._spi.transfer([val])
     opti_flow_cs.on()
     sleep(0.00005)
 
-def mousecam_read_reg(reg):
+
+def sensor_read_reg(reg):
     opti_flow_cs.off()
     opti_flow_sensor._spi.transfer([reg])
     sleep(0.000075)
@@ -114,6 +108,7 @@ def mousecam_read_reg(reg):
     opti_flow_cs.on()
     sleep(0.000005)
     return ret[0];
+
 
 class Move:
     def __init__(self):
@@ -124,29 +119,30 @@ class Move:
         self.shutter = 0
         self.max_pix = 0
 
-def mousecam_read_motion():
+
+def sensor_read_motion():
     opti_flow_cs.off()
     opti_flow_sensor._spi.transfer([ADNS3080_MOTION_BURST])
-    sleep(0.0001) # > 75us
+    sleep(0.0001)  # > 75us
 
     m = Move()
 
     m.motion = opti_flow_sensor._spi.transfer([0xff])[0]
 
-    dx =  opti_flow_sensor._spi.transfer([0xff])[0]
+    dx = opti_flow_sensor._spi.transfer([0xff])[0]
     if dx > 127:
         dx = (255 - dx) * -1
 
-    dy =  opti_flow_sensor._spi.transfer([0xff])[0]
+    dy = opti_flow_sensor._spi.transfer([0xff])[0]
     if dy > 127:
         dy = (255 - dy) * -1
 
     m.dx = dx
     m.dy = dy
-    m.squal =  opti_flow_sensor._spi.transfer([0xff])[0]
-    m.shutter =  opti_flow_sensor._spi.transfer([0xff])[0] << 8
-    m.shutter |=  opti_flow_sensor._spi.transfer([0xff])[0]
-    m.max_pix =  opti_flow_sensor._spi.transfer([0xff])[0]
+    m.squal = opti_flow_sensor._spi.transfer([0xff])[0]
+    m.shutter = opti_flow_sensor._spi.transfer([0xff])[0] << 8
+    m.shutter |= opti_flow_sensor._spi.transfer([0xff])[0]
+    m.max_pix = opti_flow_sensor._spi.transfer([0xff])[0]
 
     opti_flow_cs.on()
     sleep(0.000005)
@@ -159,8 +155,8 @@ def get_data():
 
     seq += 1
 
-    m = mousecam_read_motion()
-    #if m.motion:
+    m = sensor_read_motion()
+    # if m.motion:
     abs_x += m.dx
     abs_y += m.dy
 
@@ -184,7 +180,7 @@ def get_data():
     odomPub.publish(msg)
 
     sleep(0.05)
-    #rate.sleep()
+    # rate.sleep()
 
 
 # Setup the sensor
@@ -197,8 +193,8 @@ while not rospy.is_shutdown():
         get_data()
 
     except (KeyboardInterrupt, SystemExit):
-        #opti_flow_sensor.close()
+        # opti_flow_sensor.close()
         raise
     except:
-        #opti_flow_sensor.close()
+        # opti_flow_sensor.close()
         traceback.print_exc()
