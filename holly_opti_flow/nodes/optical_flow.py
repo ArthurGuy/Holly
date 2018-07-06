cf855f768fe06f11703b58c70a017a95f30ce1d0
Arthur Guy <arthur@assys.net>
2018-07-06 23:06:24 +0100

    no message

--- holly_opti_flow/nodes/optical_flow.py
+++ holly_opti_flow/nodes/optical_flow.py
@@ -169,7 +169,7 @@ def get_data():
     abs_x_m = (float(abs_x) / ADNS3080_COUNTS_PER_METER)
     abs_y_m = (float(abs_y) / ADNS3080_COUNTS_PER_METER)
 
-    print str(abs_x) + ", " + str(abs_y)
+    # print str(abs_x) + ", " + str(abs_y)
     print str(abs_x_m) + ", " + str(abs_y_m)
     print m.squal
 
