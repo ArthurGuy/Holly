dd92bf48bc43e21d8b75c0489d7dc0f9c6664636
Arthur Guy <arthur@assys.net>
2018-09-21 16:58:45 +0100

    read the adc

--- holly_tf/nodes/bogie-position.py
+++ holly_tf/nodes/bogie-position.py
@@ -3,6 +3,8 @@ import rospy
 import traceback
 import sys, getopt
 import random
+import Adafruit_ADS1x15
+import time
 from sensor_msgs.msg import JointState
 sys.path.append('.')
 
@@ -14,11 +16,25 @@ jointMessage = JointState()
 
 seq = 1
 
+# Note you can change the I2C address from its default (0x48), and/or the I2C
+# bus by passing in these optional parameters:
+#adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+adc = Adafruit_ADS1x15.ADS1115()
+
+# Gain
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
 
 def get_data():
     global seq
 
     # Read sensor data
+    rear = adc.read_adc(0, gain=1)
+    left = adc.read_adc(1, gain=1)
+    right = adc.read_adc(2, gain=1)
 
     seq += 1
 
