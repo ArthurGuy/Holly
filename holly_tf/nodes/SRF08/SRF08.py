271d09e84ec79f163f9fc215726caaf97b3cd222
Arthur Guy <arthur@assys.net>
2018-09-24 13:06:43 +0100

    refactor

--- holly_tf/nodes/SRF08/SRF08.py
+++ holly_tf/nodes/SRF08/SRF08.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python
 
-import smbus
+import Adafruit_GPIO.I2C as I2C
 import time
 
 
 class SRF08:
-    device = smbus.SMBus(1)
-    address = None
     delay_time = 0.1
 
-    def __init__(self, dev_addr):
-        self.address = dev_addr
+    def __init__(self, address=0xE0, busnum=I2C.get_default_bus()):
+        # Create I2C device.
+        self._device = I2C.Device(address, busnum)
 
     def distance(self):
-        self.__write_i2c(0x51)
+        self._device.write8(0, 0x51)
         time.sleep(self.delay_time)
-        range_byte_1 = self.__read_i2c(2)
-        range_byte_2 = self.__read_i2c(3)
+        range_byte_1 = self._device.readU8(2)
+        range_byte_2 = self._device.readU8(3)
 
         distance = (range_byte_1 << 8) + range_byte_2
         return distance
-
-    def light_level(self):
-        self.__write_i2c(self.delay_time)
-        time.sleep(0.1)
-        return self.__read_i2c(1)
-
-    def __write_i2c(self, data):
-        self.device.write_byte_data(self.address, 0, data)
-        return 0
-
-    def __read_i2c(self, reg):
-        data = self.device.read_byte_data(self.address, reg)
-        return data
