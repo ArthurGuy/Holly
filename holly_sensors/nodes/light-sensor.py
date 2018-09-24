818b0756fd845eacef9d2968955f5304d815d2df
Arthur Guy <arthur@assys.net>
2018-09-24 11:20:47 +0100

    tidy and reduce update rate

--- holly_sensors/nodes/light-sensor.py
+++ holly_sensors/nodes/light-sensor.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 import rospy
 import traceback
-import sys, getopt
+import sys
 import SI1145.SI1145 as SI1145
-import time
 from std_msgs.msg import Float64, Float32
 from holly_sensors.msg import LightReading
 sys.path.append('.')
 
-rospy.init_node('holly_light_sensor') #public display name of the publisher
-rate = rospy.Rate(1) # 1hz
+rospy.init_node('holly_light_sensor')  # public display name of the publisher
+rate = rospy.Rate(0.1)  # 0.1hz
 
 lightPublisher = rospy.Publisher('/holly/light_sensor', Float32, queue_size=10)
 lightMessage = Float32()
 
-seq = 1
-
 sensor = SI1145.SI1145()
 sensorSetupNeeded = 0
 
+
 def get_data():
-    global seq, sensorSetupNeeded
+    global sensorSetupNeeded
 
     try:
 
@@ -31,12 +29,6 @@ def get_data():
         print 'Vis:             ' + str(vis)
         print 'IR:              ' + str(IR)
         print 'UV Index:        ' + str(uvIndex)
-
-        seq += 1
-
-        # lightMessage.header.seq = seq
-        # lightMessage.header.stamp = rospy.Time.now()
-        # lightMessage.header.frame_id = "base_link"
 
         # lightMessage.vis = vis
         # lightMessage.ir = IR
