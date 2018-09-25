ca54c99efbbac76283504740e3bdc76c945db7de
Arthur Guy <arthur@assys.net>
2018-09-26 00:57:49 +0100

    different sensor library

--- holly_tf/nodes/chassis-height.py
+++ holly_tf/nodes/chassis-height.py
@@ -2,7 +2,7 @@
 import rospy
 import traceback
 import sys
-import VL53L1X
+import VL53L0X
 from sensor_msgs.msg import Range
 from time import sleep
 sys.path.append('.')
@@ -26,9 +26,8 @@ def setup_sensor():
     if device_setup:
         return
 
-    tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
-    tof.open() # Initialise the i2c bus and configure the sensor
-    tof.start_ranging(1)  # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
+    tof = VL53L0X.VL53L0X()
+    tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
     device_setup = 1
 
     print "Device setup"
