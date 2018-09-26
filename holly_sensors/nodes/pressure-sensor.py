3c7d4fbeb32c50b99685a438ab633d60a2b07fc9
Arthur Guy <arthur@assys.net>
2018-09-26 11:06:41 +0100

    setup

--- holly_sensors/nodes/pressure-sensor.py
+++ holly_sensors/nodes/pressure-sensor.py
@@ -13,30 +13,35 @@ rate = rospy.Rate(0.5)  # 0.1hz
 pressurePublisher = rospy.Publisher('/holly/pressure_sensor', Float32, queue_size=10)
 pressureMessage = Float32()
 
-sensorSetupNeeded = 1
+try:
+    sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+    sensorSetupNeeded = 0
+except IOError:
+    print 'Error setting up sensor'
+    sensorSetupNeeded = 1
 
 
 def get_data():
     global sensorSetupNeeded
 
-    try:
+    if not sensorSetupNeeded:
+        try:
+            degrees = sensor.read_temperature()
+            pascals = sensor.read_pressure()
+            hectopascals = pascals / 100
+            humidity = sensor.read_humidity()
 
-        degrees = sensor.read_temperature()
-        pascals = sensor.read_pressure()
-        hectopascals = pascals / 100
-        humidity = sensor.read_humidity()
+            print 'Temp      = {0:0.3f} deg C'.format(degrees)
+            print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+            print 'Humidity  = {0:0.2f} %'.format(humidity)
 
-        print 'Temp      = {0:0.3f} deg C'.format(degrees)
-        print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-        print 'Humidity  = {0:0.2f} %'.format(humidity)
+            pressureMessage.data = round(hectopascals, 2)
 
-        pressureMessage.data = round(hectopascals, 2)
-
-        pressurePublisher.publish(pressureMessage)
-    except IOError:
-        print 'Error reading sensor'
-        sensorSetupNeeded = 1
-        # sleep(5)
+            pressurePublisher.publish(pressureMessage)
+        except IOError:
+            print 'Error reading sensor'
+            sensorSetupNeeded = 1
+            # sleep(5)
 
     rate.sleep()
 
