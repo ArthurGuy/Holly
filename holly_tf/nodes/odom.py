8417a204b08e9a351660c001f72767a88bb567ae
Arthur Guy <arthur@assys.net>
2018-10-04 23:21:07 +0100

    no message

--- holly_tf/nodes/odom.py
+++ holly_tf/nodes/odom.py
@@ -104,7 +104,7 @@ def update_position():
         odomMsg.header.seq = seq
         odomMsg.header.stamp = rospy.Time.now()
         odomMsg.header.frame_id = "odom"
-        odomMsg.child_frame_id = "base_footprint"
+        odomMsg.child_frame_id = "base_link"
 
         pose = Pose()
         pose.position.x = abs_x_m
