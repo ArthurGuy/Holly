f1931950120b0e0838baede396759cd86bf151a6
Arthur Guy <arthur@assys.net>
2018-10-15 20:46:49 +0100

    Delay launch untill the master is runing

--- holly2-start.sh
+++ holly2-start.sh
@@ -3,4 +3,4 @@
 export ROS_IP=192.168.1.61
 export ROS_MASTER_URI=http://192.168.1.60:11311
 source /home/holly/catkin_ws/devel/setup.bash
-roslaunch holly start2.launch
+roslaunch holly start2.launch --wait
